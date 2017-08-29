0d34d4dc000787165a6e122fae93180fb57c67ca
NxDon <645104928@qq.com>
2017-08-29 14:25:44 +0800

    add README.md

--- src/Menu/views/Menu.js
+++ src/Menu/views/Menu.js
@@ -3,12 +3,13 @@ import {Link} from 'react-router';
 
 const Menu = () => {
     return (
-        <div>
+        <navbar>
+            <h2>学生成绩管理系统</h2>
             <ul>
-                <li><Link to="/add">Home</Link></li>
-                <li><Link to="/show">About</Link></li>
+                <li><Link to="/add">添加学生成绩</Link></li>
+                <li><Link to="/show">查看学生成绩</Link></li>
             </ul>
-        </div>
+        </navbar>
     )
 }
 export {Menu}
